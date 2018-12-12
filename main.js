c1913de5116b607e954819732eadc3bf25eddf14
Cheulong Sear <long@buzzwoo.de>
2018-12-12 11:17:42 +0700

    add shadow

--- main.js
+++ main.js
@@ -14,19 +14,28 @@ function init() {
   camera.position.y = 40;
   camera.position.z = 30;
   camera.lookAt(scene.position);
+
   //Render
   var renderer = new THREE.WebGLRenderer();
   renderer.setClearColor(new THREE.Color(0xeeeeee));
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMapEnabled = true;
 
-  //Add axes
-  var axes = new THREE.AxesHelper(20);
+  // //Add axes
+  var axes = new THREE.AxisHelper(20);
   scene.add(axes);
+
+  //Add SpotLight
+  var spotLight = new THREE.SpotLight(0xffffff);
+  spotLight.castShadow = true;
+  spotLight.position.set(-40, 60, -10);
+  scene.add(spotLight);
 
   //Add Plane
   var planeGeometry = new THREE.PlaneGeometry(60, 20);
-  var planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
+  var planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc });
   var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.receiveShadow = true;
   plane.rotation.x = -0.5 * Math.PI;
   plane.position.x = 15;
   plane.position.y = 0;
@@ -35,11 +44,12 @@ function init() {
 
   //Add Cube
   var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-  var cubeMaterial = new THREE.MeshBasicMaterial({
+  var cubeMaterial = new THREE.MeshLambertMaterial({
     color: 0xff0000,
-    wireframe: true
+    wireframe: false
   });
   var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  cube.castShadow = true;
   cube.position.x = -4;
   cube.position.y = 3;
   cube.position.z = 0;
@@ -47,11 +57,12 @@ function init() {
 
   //Add Sphere
   var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-  var sphereMaterial = new THREE.MeshBasicMaterial({
+  var sphereMaterial = new THREE.MeshLambertMaterial({
     color: 0x7777ff,
-    wireframe: true
+    wireframe: false
   });
   var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.castShadow = true;
   sphere.position.x = 20;
   sphere.position.y = 4;
   sphere.position.z = 2;
@@ -59,7 +70,6 @@ function init() {
 
   document.getElementById("WebGL-output").appendChild(renderer.domElement);
   renderer.render(scene, camera);
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
 }
 
 window.onload = init;
