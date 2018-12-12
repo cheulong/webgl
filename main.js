600fddc666c2a52bc67be75a90d8f4182ccc2de5
Cheulong Sear <long@buzzwoo.de>
2018-12-12 16:12:04 +0700

    add shadow

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function init() {
     0.1,
     1000
   );
-
+  var controls = new THREE.OrbitControls(camera);
   camera.position.x = -30;
   camera.position.y = 40;
   camera.position.z = 30;
@@ -27,9 +27,12 @@ function init() {
   renderer.setClearColor(new THREE.Color(0xeeeeee));
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.shadowMapEnabled = true;
+  renderer.shadowMapSoft = true;
+
+  // renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
   // //Add axes
-  var axes = new THREE.AxisHelper(20);
+  var axes = new THREE.AxesHelper(20);
   scene.add(axes);
 
   // add subtle ambient lighting
@@ -61,8 +64,13 @@ function init() {
   //Add SpotLight
   var spotLight = new THREE.SpotLight(0xffffff);
   spotLight.castShadow = true;
+  spotLight.shadowCameraVisible = true;
+  spotLight.shadow.mapSize.x = 4096; // default
+  spotLight.shadow.mapSize.y = 4096; // default
+  spotLight.shadow.camera.near = 0.5; // default
+  spotLight.shadow.camera.far = 500;
   spotLight.position.set(-40, 60, -10);
-  // scene.add(spotLight);
+  scene.add(spotLight);
 
   //Add Plane
   var planeGeometry = new THREE.PlaneGeometry(60, 20);
@@ -86,16 +94,21 @@ function init() {
   cube.castShadow = true;
   cube.position.x = -4;
   cube.position.y = 3;
-  cube.position.z = 0;
+  cube.position.z = 10;
   scene.add(cube);
 
   //Add Sphere
   var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
   var sphereMaterial = new THREE.MeshLambertMaterial({
-    color: 0x7777ff,
-    wireframe: false
+    color: 0x7777ff
   });
-  var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+  var material2 = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true
+  });
+
+  var sphere = new THREE.Mesh(sphereGeometry, [sphereMaterial, material2]);
   sphere.castShadow = true;
   sphere.position.x = 20;
   sphere.position.y = 4;
@@ -115,6 +128,10 @@ function init() {
     this.positionX = 0;
     this.positionY = 10;
     this.positionZ = 0;
+    this.shadowWidth = 512; // default
+    this.shadowHeight = 512; // default
+    this.shadowNear = 0.5; // default
+    this.shadowFar = 500;
   }();
 
   var gui = new dat.GUI();
@@ -153,6 +170,20 @@ function init() {
     sphereLightMesh.position.z = e;
   });
 
+  guiSpot = gui.addFolder("SpotLight");
+  guiSpot.add(controls, "shadowWidth", 0, 4096).onChange(function(e) {
+    spotLight.shadow.mapSize.x = e;
+  });
+  guiSpot.add(controls, "shadowHeight", 0, 4096).onChange(function(e) {
+    spotLight.shadow.mapSize.y = e;
+  });
+  guiSpot.add(controls, "shadowNear", 0, 30).onChange(function(e) {
+    spotLight.shadow.camera.near = e;
+  });
+  guiSpot.add(controls, "shadowFar", 30, 1000).onChange(function(e) {
+    spotLight.shadow.camera.far = e;
+  });
+
   animate();
 
   function initStats() {
@@ -178,7 +209,7 @@ function init() {
     cube.rotation.x += 0.02;
     cube.rotation.y += 0.02;
     cube.rotation.z += 0.02;
-
+    sphere.rotation.y += 0.02;
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
   }
